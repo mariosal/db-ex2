8ad019187edba2a8765eddaa608a12ecd40c7e76
Nikos Iosifidis <n1kosdesola@gmail.com>
2017-01-09 23:32:51 +0200

    sizeof fix with strlen

--- src/record.c
+++ src/record.c
@@ -4,12 +4,12 @@
 void RecordInitialize(struct Record* record, int id, const char* name,
                       const char* surname, const char* city) {
   record->id = id;
-  strncpy(record->name, name, sizeof(record->name) - 1);
-  strncpy(record->surname, surname, sizeof(record->surname) - 1);
-  strncpy(record->city, city, sizeof(record->city) - 1);
-  record->name[sizeof(record->name) - 1] = '\0';
-  record->surname[sizeof(record->surname) - 1] = '\0';
-  record->city[sizeof(record->city) - 1] = '\0';
+  strncpy(record->name, name,strlen(record->name) - 1);
+  strncpy(record->surname, surname,strlen(record->surname) - 1);
+  strncpy(record->city, city, strlen(record->city) - 1);
+  record->name[strlen(record->name) - 1] = '\0';
+  record->surname[strlen(record->surname) - 1] = '\0';
+  record->city[strlen(record->city) - 1] = '\0';
 }
 
 int RecordPrint(const struct Record* record, FILE* out) {
