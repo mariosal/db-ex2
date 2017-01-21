d68a5f0ae76042ac06ebec3ba46a11fb79a9b958
Mario Saldinger <mariosaldinger@gmail.com>
2017-01-21 02:31:24 +0200

    Fix sorted.h prototypes' return type

--- include/sorted.h
+++ include/sorted.h
@@ -3,4 +3,12 @@
 
 #include "record.h"
 
+int Sorted_CreateFile(const char* filename);
+int Sorted_OpenFile(const char* filename);
+int Sorted_CloseFile(int fd);
+int Sorted_InsertEntry(int fd, struct Record Record);
+int Sorted_SortFile(const char* filename, int field_no);
+int Sorted_checkSortedFile(const char* filename, int fieldno);
+void Sorted_GetAllEntries(int fd, const int* fiedno, const void* value);
+
 #endif  // SORTED_H_
