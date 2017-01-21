d2ca929bfcfca4948744eb1cf159012e01b861e7
Mario Saldinger <mariosaldinger@gmail.com>
2017-01-21 04:33:42 +0200

    Fix sorted.h function parameter name

--- include/sorted.h
+++ include/sorted.h
@@ -6,7 +6,7 @@
 int Sorted_CreateFile(const char* filename);
 int Sorted_OpenFile(const char* filename);
 int Sorted_CloseFile(int fd);
-int Sorted_InsertEntry(int fd, struct Record Record);
+int Sorted_InsertEntry(int fd, struct Record record);
 int Sorted_SortFile(const char* filename, int field_no);
 int Sorted_checkSortedFile(const char* filename, int fieldno);
 void Sorted_GetAllEntries(int fd, const int* fiedno, const void* value);
