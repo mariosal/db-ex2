3e434fb655215773a2c80332c8220c5c6d4a8288
Mario Saldinger <mariosaldinger@gmail.com>
2017-01-21 04:38:47 +0200

    Sorted: Add InsertEntry and rework helper functions and naming

--- src/sorted.c
+++ src/sorted.c
@@ -13,58 +13,57 @@ static int Sorted_MaxEntries(void) {
   return BLOCK_SIZE / sizeof(struct Record);
 }
 
-static void Sorted_SetHeap(void* block, bool heap) {
-  block = (unsigned char*)block + offsetof(struct Sorted_Info, heap);
-  memcpy(block, &heap, sizeof(heap));
+static void Sorted_SetHeap(void* zero, bool heap) {
+  zero = (unsigned char*)zero + offsetof(struct Sorted_Info, heap);
+  memcpy(zero, &heap, sizeof(heap));
 }
 
-static bool Sorted_Heap(void* block) {
-  bool heap;
-  block = (unsigned char*)block + offsetof(struct Sorted_Info, heap);
-  memcpy(&heap, block, sizeof(heap));
-  return heap;
+static bool Sorted_Heap(void* zero) {
+  return ((const struct Sorted_Info*)zero)->heap;
 }
 
-static void Sorted_SetNumEntries(void* block, int num_entries) {
-  block = (unsigned char*)block + offsetof(struct Sorted_Info, num_entries);
-  memcpy(block, &num_entries, sizeof(num_entries));
+static void Sorted_SetNumEntries(void* zero, int num_entries) {
+  zero = (unsigned char*)zero + offsetof(struct Sorted_Info, num_entries);
+  memcpy(zero, &num_entries, sizeof(num_entries));
 }
 
-static int Sorted_NumEntries(void* block) {
-  int num_entries;
-  block = (unsigned char*)block + offsetof(struct Sorted_Info, num_entries);
-  memcpy(&num_entries, block, sizeof(num_entries));
-  return num_entries;
+static int Sorted_NumEntries(void* zero) {
+  return ((const struct Sorted_Info*)zero)->num_entries;
+}
+
+static void Sorted_SetEntry(void* block, int offset, const struct Record* record) {
+  block = (char*)block + sizeof(struct Record) * offset;
+  memcpy(block, record, sizeof(struct Record));
 }
 
 int Sorted_CreateFile(const char* filename) {
-  if (BF_CreateFile(filename) == -1) {
+  if (BF_CreateFile(filename) < 0) {
     BF_PrintError("Error creating file");
     return -1;
   }
   int fd = BF_OpenFile(filename);
-  if (fd == -1) {
+  if (fd < 0) {
     BF_PrintError("Error opening file");
     return -1;
   }
 
-  if (BF_AllocateBlock(fd) == -1) {
+  if (BF_AllocateBlock(fd) < 0) {
     BF_PrintError("Error allocating block");
     return -1;
   }
-  void* block;
-  if (BF_ReadBlock(fd, 0, &block) == -1) {
+  void* zero;
+  if (BF_ReadBlock(fd, 0, &zero) < 0) {
     BF_PrintError("Error reading block");
     return -1;
   }
-  Sorted_SetHeap(block, true);
-  Sorted_SetNumEntries(block, 0);
-  if (BF_WriteBlock(fd, 0) == -1) {
+  Sorted_SetHeap(zero, true);
+  Sorted_SetNumEntries(zero, 0);
+  if (BF_WriteBlock(fd, 0) < 0) {
     BF_PrintError("Error writing block");
     return -1;
   }
 
-  if (BF_CloseFile(fd) == -1) {
+  if (BF_CloseFile(fd) < 0) {
     BF_PrintError("Error closing file");
     return -1;
   }
@@ -73,7 +72,7 @@ int Sorted_CreateFile(const char* filename) {
 
 int Sorted_OpenFile(const char* filename) {
   int fd = BF_OpenFile(filename);
-  if (fd == -1) {
+  if (fd < 0) {
     BF_PrintError("Error opening file");
     return -1;
   }
@@ -81,15 +80,15 @@ int Sorted_OpenFile(const char* filename) {
     return -1;
   }
 
-  void* block;
-  if (BF_ReadBlock(fd, 0, &block) == -1) {
+  void* zero;
+  if (BF_ReadBlock(fd, 0, &zero) < 0) {
     BF_PrintError("Error reading block");
     return -1;
   }
-  if (!Sorted_Heap(block) || Sorted_NumEntries(block) < 0) {
+  if (!Sorted_Heap(zero) || Sorted_NumEntries(zero) < 0) {
     return -1;
   }
-  int num_blocks = 1 + Sorted_NumEntries(block) / Sorted_MaxEntries();
+  int num_blocks = 1 + Sorted_NumEntries(zero) / Sorted_MaxEntries();
   if (num_blocks != BF_GetBlockCounter(fd)) {
     return -1;
   }
@@ -98,14 +97,45 @@ int Sorted_OpenFile(const char* filename) {
 }
 
 int Sorted_CloseFile(int fd) {
-  if (BF_CloseFile(fd) == -1) {
+  if (BF_CloseFile(fd) < 0) {
     BF_PrintError("Error closing file");
     return -1;
   }
   return 0;
 }
 
-int Sorted_InsertEntry(int fd, struct Record Record) {
+int Sorted_InsertEntry(int fd, struct Record record) {
+  void* zero;
+  if (BF_ReadBlock(fd, 0, &zero) < 0) {
+    BF_PrintError("Error reading block");
+    return -1;
+  }
+  if (Sorted_NumEntries(zero) % Sorted_MaxEntries() == 0) {
+    if (BF_AllocateBlock(fd) < 0) {
+      BF_PrintError("Error allocating block");
+      return -1;
+    }
+  }
+
+  void* back;
+  int back_id = BF_GetBlockCounter(fd) - 1;
+  if (BF_ReadBlock(fd, back_id, &back) < 0) {
+    BF_PrintError("Error reading block");
+    return -1;
+  }
+
+  int offset = Sorted_NumEntries(zero) - (back_id - 1) * Sorted_MaxEntries();
+  Sorted_SetEntry(back, offset, &record);
+  if (BF_WriteBlock(fd, back_id) < 0) {
+    BF_PrintError("Error writing block");
+    return -1;
+  }
+
+  Sorted_SetNumEntries(zero, Sorted_NumEntries(zero) + 1);
+  if (BF_WriteBlock(fd, 0) < 0) {
+    BF_PrintError("Error writing block");
+    return -1;
+  }
   return 0;
 }
 
