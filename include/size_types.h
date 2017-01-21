7e76f3097c6e3dab1933ba69a2842e2e93e1e30f
Mario Saldinger <mariosaldinger@gmail.com>
2017-01-21 02:29:43 +0200

    Add size_types.h to have PRIuS

---
+++ include/size_types.h
@@ -0,0 +1,24 @@
+#ifndef SIZE_TYPES_H_
+#define SIZE_TYPES_H_
+
+#include <stddef.h>
+
+// printf macros for size_t, in the style of inttypes.h
+#ifdef _LP64
+#define __PRIS_PREFIX "z"
+#else
+#define __PRIS_PREFIX
+#endif
+
+// Use these macros after a % in a printf format string to get correct 32/64 bit
+// behavior, like this:
+// size_t size = records.size();
+// printf("%" PRIuS "\n", size);
+
+#define PRIdS __PRIS_PREFIX "d"
+#define PRIxS __PRIS_PREFIX "x"
+#define PRIuS __PRIS_PREFIX "u"
+#define PRIXS __PRIS_PREFIX "X"
+#define PRIoS __PRIS_PREFIX "o"
+
+#endif  // SIZE_TYPES_H_
